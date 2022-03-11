bb0e750f59a311037b3dc0646dd8618d2a0f35f6
Andres Giraldo L <andres.giraldolondono@endava.com>
2022-03-11 11:46:53 -0500

    changes in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import Layout from "../components/Layout/layout"
+import { Link } from "gatsby"
 
 class IndexPage extends Component {
   state = {
@@ -25,14 +26,13 @@ class IndexPage extends Component {
     document.getElementById("modal").style.display = "none"
   }
   render() {
-    let indexData = this.props.data.allIndexJson.nodes[0]
-    let homeItems = []
-    let i = 0
-    indexData.home_items.forEach(item => {
+    let indexData = this.props.data.allIndexJson.nodes[0];
+    let homeItems = [];
+
+    indexData.home_alliance.forEach((item, index) => {
       homeItems.push(
         <div
-          key={i}
-          onClick={this.openModal.bind(this, item)}
+          key={index}
           className={
             this.state.smallScreen ? "grid-item-small" : "home-grid-item"
           }
@@ -42,17 +42,22 @@ class IndexPage extends Component {
           }}
         ></div>
       )
-      i++
     })
     return (
       <Layout page={"home"}>
-        <h1 className="title">{indexData.title}</h1>
-        <h2 className="subtitle">{indexData.description}</h2>
+        <div className="home-bg">
+          <h1>{indexData.title}</h1>
+          <h2>{indexData.description}</h2>
+          <Link to="/about">
+            <button>{indexData.button}</button>
+          </Link>
+        </div>
         <div className="home-main">
           <div className="text">{indexData.text}</div>
-          <div className="divider"></div>
-          <h3 className="subtitle">{indexData.subtitle}</h3>
-          <div
+        </div>
+        <div className="divider"></div>
+        <h3 className="subtitle">{indexData.subtitle}</h3>
+        <div
             className={
               this.state.smallScreen
                 ? "grid-container-small"
@@ -60,9 +65,24 @@ class IndexPage extends Component {
             }
           >
             {homeItems}
-          </div>
         </div>
-        <div id="modal" className="modal" onClick={this.closeModal}>
+        {/* <h1 className="title">{indexData.title}</h1>
+        <h2 className="subtitle">{indexData.description}</h2>
+        <div className="home-main">
+          <div className="text">{indexData.text}</div>
+          <div className="divider"></div>
+          <h3 className="subtitle">{indexData.subtitle}</h3> */}
+        {/* <div
+            className={
+              this.state.smallScreen
+                ? "grid-container-small"
+                : "home-grid-container"
+            }
+          >
+            {homeItems}
+          </div> */}
+        {/* </div> */}
+        {/* <div id="modal" className="modal" onClick={this.closeModal}>
           <div
             className={
               this.state.smallScreen ? "modal-content-small" : "modal-content"
@@ -73,9 +93,6 @@ class IndexPage extends Component {
               <div className="modal-grid-item-left">
                 <span className="modal-title">{this.state.modal.name}</span>
                 <p className="modal-text">{this.state.modal.description}</p>
-                <p className="modal-text">
-                  Completed: {this.state.modal.completed}
-                </p>
               </div>
               <div className="modal-grid-item-right">
                 <img
@@ -86,7 +103,7 @@ class IndexPage extends Component {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </Layout>
     )
   }
@@ -101,10 +118,9 @@ export const query = graphql`
         description
         text
         subtitle
-        home_items {
+        button,
+        home_alliance {
           name
-          description
-          completed
           image
         }
       }
